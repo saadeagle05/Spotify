45efd6ac3b1148e9a44dc654103b9beafdd9d42d
Xyz <84581959+5qw@users.noreply.github.com>
2022-06-07 09:34:47 +0200

    Updated

--- uninstall.bat
+++ uninstall.bat
@@ -1,26 +1,26 @@
 @echo off
 echo *****************
-echo Author: @rednek46
+echo Author: @xyz
 echo *****************
-echo Removing Patch
+echo Borrando bloqueador
 if exist "%APPDATA%\Spotify\chrome_elf_bak.dll" (
     del /s /q "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
     move "%APPDATA%\Spotify\chrome_elf_bak.dll" "%APPDATA%\Spotify\chrome_elf.dll" > NUL 2>&1
 ) else (
-    echo done
+    echo Hecho
 )
 
 if exist "%APPDATA%\Spotify\Apps\zlink.spa.bak" (
     del /s /q "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
     move "%APPDATA%\Spotify\Apps\zlink.spa.bak" "%APPDATA%\Spotify\Apps\zlink.spa" > NUL 2>&1
 ) else (
-    echo done
+    echo Hecho
 )
 
 if exist "%APPDATA%\Spotify\Apps\xpui.spa.bak" (
     del /s /q "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
     move "%APPDATA%\Spotify\Apps\xpui.spa.bak" "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
 ) else (
-    echo done
+    echo Hecho
 )
 pause
